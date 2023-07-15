776925c92da7c47c9ed2c88a2bd5c69c6bbc8856
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-07-15 16:31:14 -0300

    add a float menu

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+        port: '',
+        pathname: '/images/blocks/header.57a7576a.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+        port: '',
+        pathname: '/images/blocks/subheader.9aab4769.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+        port: '',
+        pathname: '/images/blocks/bulleted-list.0e87e917.png',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
--- src/app/(home)/components/Editor.tsx
+++ src/app/(home)/components/Editor.tsx
@@ -1,5 +1,10 @@
 'use client'
-import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react'
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { initialContent } from './initialContent'
 import { lowlight } from 'lowlight'
@@ -17,6 +22,7 @@ import {
 } from 'react-icons/rx'
 import 'highlight.js/styles/tokyo-night-dark.css'
 import { BubbleButton } from './BubbleButton'
+import Image from 'next/image'
 
 export function Editor() {
   lowlight.registerLanguage('html', html)
@@ -83,6 +89,73 @@ export function Editor() {
             </BubbleButton>
           </div>
         </BubbleMenu>
+      )}
+      {editor && (
+        <FloatingMenu
+          className="bg-zinc-50 py-2 px-1 shadow-xl border border-zinc-200 shadow-black/20 rounded-lg overflow-hidden flex flex-col"
+          editor={editor}
+          tippyOptions={{ duration: 100 }}
+          shouldShow={({ state }) => {
+            const { $from } = state.selection
+
+            const currentLineText = $from.nodeBefore?.textContent
+
+            return currentLineText === '/'
+          }}
+        >
+          <button
+            className="flex items-center gap-2 rounded min-w-[280px]"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+            data-active={editor.isActive('heading', { level: 1 })}
+          >
+            <Image
+              alt=""
+              src="https://www.notion.so/images/blocks/header.57a7576a.png"
+              width={50}
+              height={50}
+            />
+            <div>
+              <span>Heading 1</span>
+              <span>Big section heading</span>
+            </div>
+          </button>
+          <button
+            className="flex items-center gap-2 rounded min-w-[280px]"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            data-active={editor.isActive('heading', { level: 2 })}
+          >
+            <Image
+              alt=""
+              src="https://www.notion.so/images/blocks/subheader.9aab4769.png"
+              width={50}
+              height={50}
+            />
+            <div>
+              <span>Heading 2</span>
+              <span>Medium section heading</span>
+            </div>
+          </button>
+          <button
+            className="flex items-center gap-2 rounded min-w-[280px]"
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            data-active={editor.isActive('bulletList', { level: 1 })}
+          >
+            <Image
+              alt=""
+              src="https://www.notion.so/images/blocks/bulleted-list.0e87e917.png"
+              width={50}
+              height={50}
+            />
+            <div>
+              <span>Bulleted list</span>
+              <span>Create a simple bulleted list.</span>
+            </div>
+          </button>
+        </FloatingMenu>
       )}
       <EditorContent
         className="w-[700px] mx-auto p-16 prose prose-zinc"
